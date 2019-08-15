dd89357704a2f64ede61773f7753d55a113f238d
Jeri Pradita <jeri.pradita@bukalapak.com>
2019-08-15 12:15:43 +0700

    tugas SQA week1

--- lesson3/features/scenario/api.feature
+++ lesson3/features/scenario/api.feature
@@ -1,9 +1,12 @@
 Feature: Get Data from API
 
 Scenario: Fetching todos
-    Given I want to get todos with in "https://jsonplaceholder.typicode.com/todos"
-      And I want to fetch todos data with id "1"
+    Given I want to get user with in "https://jsonplaceholder.typicode.com/users"
+      And I want to fetch user data with id "2"
       And Show me the response
-    Then I should get todos with id "1"
-    And I should get todos with title "delectus aut autem"
+    Then I should get user with id "2"
+    And I should get user with name "Ervin Howell"
+    And I should get user with username "Antonette"
+    And I should get user address with street "Victor Plains"
+    And I should get user address with geo lat "-43.9509"
     Then Response status should be "200"
--- lesson3/features/step_definitions/api_steps.rb
+++ lesson3/features/step_definitions/api_steps.rb
@@ -1,24 +1,36 @@
-Given("I want to get todos with in {string}") do |url|
+Given("I want to get user with in {string}") do |url|
     @url = url
 end
 
-Given("I want to fetch todos data with id {string}") do |id|
+And("I want to fetch user data with id {string}") do |id|
     @resp = Faraday.get "#{@url}/#{id}"
     @content = JSON.parse(@resp.body)
 end
 
-Then("I should get todos with id {string}") do |id|
+Then("I should get user with id {string}") do |id|
     expect(@content['id']).to eq id.to_i
 end
 
-Then("I should get todos with title {string}") do |title|
-    expect(@content['title']).to eq title
+And("I should get user with name {string}") do |name|
+    expect(@content['name']).to eq name
 end
 
-Then("Show me the response") do
+And("I should get user with username {string}") do |username|
+  expect(@content['username']).to eq username
+end
+
+And("I should get user address with street {string}") do |street|
+  expect(@content['address']['street']).to eq street
+end
+
+And("I should get user address with geo lat {string}") do |lat|
+  expect(@content['address']['geo']['lat'].to_f).to eq lat.to_f
+end
+
+And("Show me the response") do
   puts @content
 end
 
-Then("Response status should be {string}") do |status|
+And("Response status should be {string}") do |status|
   expect(@resp.status).to eq(status.to_i)
 end
